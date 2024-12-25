1a3ebba7682a06e3ba800dcb2024896adeb932e7
fhd-codes <fhd1103@outlook.com>
2024-12-25 17:53:03 +0500

    basic setup done

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// Route handling
+app.get('/', (req, res) => {
+    res.send('<h2>Hello from Express.js server!!</h2>');
+});
+
+// Server setup
+app.listen(8080, () => {
+    console.log('server listening on port 8080');
+});
